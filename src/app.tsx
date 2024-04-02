93a8363f7ce9ed3aabf27d9d783792be9c923906
Barbosa, Valdeir <barbov1@medtronic.com>
2024-04-02 14:07:30 -0300

    first commit

---
+++ src/app.tsx
@@ -0,0 +1,20 @@
+
+import { Header } from "./components/header"
+import { AttendeeList } from "./components/attendee-list"
+
+
+
+export function App() {
+ 
+
+ return(
+    <div className="max-w-[1216px] mx-auto py-5">
+        <Header/>
+        <AttendeeList/>
+    </div>
+    
+ )
+
+}
+
+
